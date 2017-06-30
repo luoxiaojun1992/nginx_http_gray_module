7f1939791bcf7980feeedb6abe50d7cd501fd219
Gogs <gogitservice@gmail.com>
2017-07-01 01:55:59 +0800

    redis

--- ngx_http_gray_module.c
+++ ngx_http_gray_module.c
@@ -138,9 +138,7 @@ static ngx_int_t ngx_http_gray_add_variable(ngx_conf_t *cf)
 
 static ngx_int_t ngx_http_isgray_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, ngx_uint_t data)
 {
-  ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, getGrayPolicy());
-
-	if (strcmp(getGrayPolicy(), "true")) {
+	if (strcmp(getGrayPolicy(), (char *)"true")) {
 		isGray = 1;
 	} else {
 		isGray = 0;
