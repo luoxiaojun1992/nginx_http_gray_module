e420326afa3953501eb75498ba4c1d4749d54c64
Gogs <gogitservice@gmail.com>
2017-06-30 23:38:38 +0800

    add http client

--- http-client-c.h
+++ http-client-c.h
@@ -30,24 +30,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#ifdef _WIN32
-	#include <winsock2.h>
-	#include <ws2tcpip.h>
-	#include <stdio.h>
-	#pragma comment(lib, "Ws2_32.lib")
-#elif _LINUX
-	#include <sys/socket.h>
-#elif __FreeBSD__
-    #include <sys/socket.h>
-    #include <netinet/in.h>
-    #include <netdb.h>
-    #include <arpa/inet.h>
-#else
-	#error Platform not suppoted.
-#endif
-
+#include <sys/socket.h>
 #include <errno.h>
-#include "stringx.h";
+#include "stringx.h"
 #include "urlparser.h"
 
 /*
