e42d9dcf89e3633840d213d1882f0ea51f85bff2
Gogs <gogitservice@gmail.com>
2017-06-30 22:43:19 +0800

    remove useless code

--- ngx_http_gray_module.c
+++ ngx_http_gray_module.c
@@ -2,6 +2,24 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define IPSTR "61.147.124.120"
+#define PORT 80
+#define BUFSIZE 1024
+
 ngx_uint_t isGray = 0;
 
 static ngx_str_t new_variable_is_gray = ngx_string("is_gray");
@@ -25,6 +43,7 @@ static ngx_int_t ngx_http_isgray_variable(ngx_http_request_t *r, ngx_http_variab
 
 static ngx_int_t ngx_http_isnotgray_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, ngx_uint_t data);
 
+int httpRequest();
 
 /*模块 commands*/
 static ngx_command_t  ngx_http_gray_commands[] =
@@ -135,6 +154,8 @@ static ngx_int_t ngx_http_gray_add_variable(ngx_conf_t *cf)
 
 static ngx_int_t ngx_http_isgray_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, ngx_uint_t data)
 {
+	httpRequest();
+
 	if (ngx_random() % 2 == 0) {
 		isGray = 1;
 	} else {
@@ -159,4 +180,84 @@ static ngx_int_t ngx_http_isnotgray_variable(ngx_http_request_t *r, ngx_http_var
 	}
 
   return NGX_OK;
+}
+
+int httpRequest()
+{
+        int sockfd, ret, i, h;
+        struct sockaddr_in servaddr;
+        char str1[4096], str2[4096], buf[BUFSIZE], *str;
+        socklen_t len;
+        fd_set   t_set1;
+        struct timeval  tv;
+
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+                //
+        };
+
+        bzero(&servaddr, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(PORT);
+        if (inet_pton(AF_INET, IPSTR, &servaddr.sin_addr) <= 0 ){
+                //
+        };
+
+        if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+                //
+        }
+        //发送数据
+        memset(str2, 0, 4096);
+        strcat(str2, "qqCode=474497857");
+        str=(char *)malloc(128);
+        len = strlen(str2);
+        sprintf(str, "%d", len);
+
+        memset(str1, 0, 4096);
+        strcat(str1, "POST /webservices/qqOnlineWebService.asmx/qqCheckOnline HTTP/1.1\n");
+        strcat(str1, "Host: www.webxml.com.cn\n");
+        strcat(str1, "Content-Type: application/x-www-form-urlencoded\n");
+        strcat(str1, "Content-Length: ");
+        strcat(str1, str);
+        strcat(str1, "\n\n");
+
+        strcat(str1, str2);
+        strcat(str1, "\r\n\r\n");
+        printf("%s\n",str1);
+
+        ret = write(sockfd,str1,strlen(str1));
+        if (ret < 0) {
+                //
+        }else{
+                //
+        }
+
+        FD_ZERO(&t_set1);
+        FD_SET(sockfd, &t_set1);
+
+        while(1){
+                sleep(2);
+                tv.tv_sec= 0;
+                tv.tv_usec= 0;
+                h= 0;
+                h= select(sockfd +1, &t_set1, NULL, NULL, &tv);
+
+                //if (h == 0) continue;
+                if (h < 0) {
+                        close(sockfd);
+                        return -1;
+                };
+
+                if (h > 0){
+                        memset(buf, 0, 4096);
+                        i= read(sockfd, buf, 4095);
+                        if (i==0){
+                                close(sockfd);
+                                return -1;
+                        }
+                }
+        }
+        close(sockfd);
+
+
+        return 0;
 }
