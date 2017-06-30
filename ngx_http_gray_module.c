307844ab61b20ee95daa9356426e8fef0ebb356f
罗晓俊 <luoxiaojun1992@sina.cn>
2017-06-30 14:22:58 +0800

    add upstream

--- ngx_http_gray_module.c
+++ ngx_http_gray_module.c
@@ -36,7 +36,11 @@ static ngx_int_t ngx_http_isnotgray_variable(ngx_http_request_t *r, ngx_http_var
 
 static void* ngx_http_gray_create_loc_conf(ngx_conf_t *cf);
 
-static char *ngx_http_gray_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+static char *ngx_http_gray_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+
+static ngx_int_t gray_upstream_create_request(ngx_http_request_t *r);
+
+static ngx_int_t gray_process_status_line(ngx_http_request_t *r);
 
 
 
@@ -147,7 +151,104 @@ static ngx_int_t gray_upstream_create_request(ngx_http_request_t *r)
 
   b->last = b->pos + queryLineLen;
 
-  //todo
+  ngx_snprintf(b->pos, queryLineLen, (char *)backendQueryLine.data, &r->args);
+  r->upstream->request_bufs = ngx_alloc_chain_link(r->pool);
+  if (r->upstream->request_bufs == NULL) {
+    return NGX_ERROR;
+  }
+  r->upstream->request_bufs->buf = b;
+  r->upstream->request_bufs->next = NULL;
+  r->upstream->request_sent = 0;
+  r->upstream->header_sent = 0;
+  r->header_hash = 1;
+  return NGX_OK;
+}
+
+static ngx_int_t gray_process_status_line(ngx_http_request_t *r)
+{
+  size_t len;
+  ngx_int_t rc;
+  ngx_http_upstream_t *u;
+  ngx_http_gray_ctx_t* ctx = ngx_http_get_module_ctx(r, ngx_http_gray_module);
+
+  if (ctx == NULL) {
+    return NGX_ERROR;
+  }
+
+  u = r->upstream;
+
+  rc = ngx_http_parse_status_line(r, &u->buffer, &ctx->status);
+
+  if (rc == NGX_AGAIN) {
+    return rc;
+  }
+
+  if (rc == NGX_ERROR) {
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "upstream set not valid HTTP/1.0 header");
+    r->http_version = NGX_HTTP_VERSION_9;
+    u->state->status = NGX_HTTP_OK;
+    return NGX_OK;
+  }
+
+  if (u->state) {
+    u->state->status = ctx->status.code;
+  }
+
+  u->headers_in.status.n = ctx->status.code;
+
+  len = ctx->status.end - ctx->status.start;
+
+  u->headers_in.status_line.len = len;
+
+  u->headers_in.status_line.data = ngx_pnalloc(r->pool, len);
+
+  if (u->headers_in.status_line.data == NULL) {
+    return NGX_ERROR;
+  }
+
+  ngx_memcpy(u->headers_in.status_line.data, ctx->status.start, len);
+
+  u->process_header = gray_upstream_process_header;
+
+  return gray_upstream_process_header(r);
+}
+
+static ngx_int_t gray_upstream_process_header(ngx_http_request_t *r)
+{
+  ngx_int_t rc;
+  ngx_table_elt_t *h;
+  ngx_http_upstream_header_t *hh;
+  ngx_http_upstream_main_conf_t *umcf;
+
+  umcf = ngx_http_get_module_main_conf(r, ngx_http_upstream_module);
+
+  for(;;) {
+    rc = ngx_http_parse_header_line(r, &r->upstream->buffer, 1);
+
+    if (rc == NGX_OK) {
+      h = ngx_list_push(&r->upstream->headers_in.headers);
+
+      if (h == NULL) {
+        return NGX_ERROR;
+      }
+
+      h->hash = r->header_hash;
+      h->key.len = r->header_name_end - r->header_name_start;
+      h->value.len = r->header_end - r->header_start;
+      h->key.data = ngx_pnalloc(r->pool, h->key.len + 1 + h->value.len + 1 + h->key.len);
+      if (h->key.data == NULL) {
+        return NGX_ERROR;
+      }
+
+      h->value.data = h->key.data + h->key.len + 1;
+      h->lowcase_key = h->key.data + h->key.len + 1 + h->value.len + 1;
+
+      ngx_memcpy(h->key.data, r->header_name_start, h->key.len);
+      h->key.data[h->ken.len] = '\0';
+      ngx_memcpy(h->value.data, r->header_start, h->value.len);
+      //todo
+    }
+  }
 }
 
 /**
