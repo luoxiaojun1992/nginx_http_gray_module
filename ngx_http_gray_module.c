7ee2d618fe7a7461753cd4a2bc2f204b5f733a6d
罗晓俊 <luoxiaojun1992@sina.cn>
2017-07-03 11:18:37 +0800

    bugfix

--- ngx_http_gray_module.c
+++ ngx_http_gray_module.c
@@ -140,7 +140,7 @@ static ngx_int_t ngx_http_gray_add_variable(ngx_conf_t *cf)
 
 static ngx_int_t ngx_http_isgray_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, ngx_uint_t data)
 {
-  getGrayPolicy();
+  getGrayPolicy(r);
 
 	// if (ngx_random() % 2 == 0) {
 	// 	isGray = 1;
