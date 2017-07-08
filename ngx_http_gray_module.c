12ea619034e108fc5bb34cf1aa60b8a3924d4f80
Gogs <gogitservice@gmail.com>
2017-07-08 13:22:18 +0800

    add arguments

--- ngx_http_gray_module.c
+++ ngx_http_gray_module.c
@@ -193,10 +193,10 @@ int getGrayPolicy(ngx_http_request_t *r)
 
   redisContext *c;
   redisReply *reply;
-  // const char *hostname = DEFAULT_REDIS_HOST;
+  const char *hostname = (char *)elcf->redis_host.data;
   int port = elcf->redis_port;
   struct timeval timeout = { 1, 500000 }; // 1.5 seconds
-  c = redisConnectWithTimeout(elcf->redis_host.data, port, timeout);
+  c = redisConnectWithTimeout(hostname, port, timeout);
   if (c == NULL || c->err) {
       if (c) {
           ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Connection error: %s", c->errstr);
